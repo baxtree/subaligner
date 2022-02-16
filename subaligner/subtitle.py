224099c6a56158f5355dfeefc34b433596f674e3
baxtree <xi.bai.ed@gmail.com>
2022-02-16 18:26:33 +0000

    try to fix type checks

--- subaligner/subtitle.py
+++ subaligner/subtitle.py
@@ -741,7 +741,7 @@ class Subtitle(object):
                 # Change single quotes in the XML header to double quotes
                 with open(target_file_path, "w", encoding=encoding) as target:
                     if "xml_declaration" in inspect.getfullargspec(ElementTree.tostring).kwonlyargs:  # for >= python 3.8
-                        encoded = ElementTree.tostring(tt, encoding=encoding, method="xml", xml_declaration=True)
+                        encoded = ElementTree.tostring(tt, encoding=encoding, method="xml", xml_declaration=True) # type: ignore
                     else:
                         encoded = ElementTree.tostring(tt, encoding=encoding, method="xml")
                     normalised = encoded.decode(encoding) \
